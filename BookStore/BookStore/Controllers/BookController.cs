19c26e3ea2df972aa8f24ab35e6d4727680041d3
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-06 21:14:02 +0600

    Added Book Controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository  _bookRepository= null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public List<Book> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public Book GetBook(int id)
+        {
+            return _bookRepository.GetBook(id);
+        }
+
+        public List<Book> SearchBook(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+
+    }
+}
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<Book> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public Book GetBook(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<Book> SearchBook(string bookName, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(bookName) || x.Author.Contains(authorName)).ToList();
+        }
+
+        private List <Book> DataSource()
+        {
+            return new List<Book>()
+            {
+                new Book(){Id = 1, Title="MVC", Author="Nazim"},
+                new Book(){Id = 2, Title="C#", Author="Nazim"},
+                new Book(){Id = 3, Title="JAVA", Author="Nazim"},
+                new Book(){Id = 4, Title="Angular", Author="Nazim"},
+            };
+        }
+    }
+}
