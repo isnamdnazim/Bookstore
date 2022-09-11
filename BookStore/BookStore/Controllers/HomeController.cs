df64cc720713a30995e44ad890c189b5e4d28c62
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-11 22:16:56 +0600

    Learning usages of ViewBag

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class HomeController: Controller 
     {
         public ViewResult Index()
-        { 
+        {
+            ViewBag.Title = "Nazim";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "Nazim";
+            ViewBag.Data = data;
+            ViewBag.Type = new Book() { Id=5,Author = "This is Author" };
             return View();
         }
         public ViewResult AboutUs()
