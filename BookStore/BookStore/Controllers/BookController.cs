36d24541598ee322edf184f36940345878c1aeae
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-10-03 00:53:08 +0600

    Adding imageUrl for book cover Photo is working

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -1,10 +1,12 @@
 ﻿using BookStore.Models;
 using BookStore.Repository;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +16,13 @@ namespace BookStore.Controllers
     {
         private readonly BookRepository  _bookRepository= null;
         private readonly LanguageRepository  _languageRepository= null;
-        public BookController(BookRepository bookRepository, LanguageRepository languageRepository)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public BookController(BookRepository bookRepository, LanguageRepository languageRepository, 
+            IWebHostEnvironment webHostEnvironment)
         {
             _bookRepository = bookRepository;
             _languageRepository = languageRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
         public async Task<ViewResult> GetAllBooks()
         {
@@ -58,6 +63,15 @@ namespace BookStore.Controllers
         {
             if (ModelState.IsValid)
             {
+                if(book.CoverPhoto != null)
+                {
+                    string folder = "books/cover/";
+                    folder += book.CoverPhoto.FileName+Guid.NewGuid().ToString();
+                    string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
+
+                    await book.CoverPhoto.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+                }
+
                 int id = await _bookRepository.AddNewBook(book);
                 if (id > 0)
                 {
